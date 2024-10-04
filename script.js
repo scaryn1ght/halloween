589bc0ade2d24d55115673fb27daeeef5c839cad
scaryn1ght <joaomotaa7546@gmail.com>
2024-10-04 16:06:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ function toggleDetails(elementId) {
 
     if (details.classList.contains("details-hidden")) {
         details.classList.remove("details-hidden");
-        arrow.textContent = "˄"; // Mudança para indicar que a descrição está visível
+        arrow.textContent = "˄"; // Indica que está visível
     } else {
         details.classList.add("details-hidden");
-        arrow.textContent = "˅"; // Mudança para indicar que a descrição está oculta
+        arrow.textContent = "˅"; // Indica que está oculto
     }
 }
 
-// Adicionando eventos de clique nos itens da lista
 document.addEventListener("DOMContentLoaded", function() {
     const fantasyItem = document.querySelector(".entretenimento ul li:nth-child(1)");
     const treasureItem = document.querySelector(".entretenimento ul li:nth-child(2)");
+    const karaokeItem = document.querySelector(".entretenimento ul li:nth-child(3)");
 
     fantasyItem.addEventListener("click", function() {
         toggleDetails("fantasy-details");
@@ -22,5 +22,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
     treasureItem.addEventListener("click", function() {
         toggleDetails("treasure-details");
+    });
+
+    karaokeItem.addEventListener("click", function() {
+        toggleDetails("karaoke-details");
     });
 });
